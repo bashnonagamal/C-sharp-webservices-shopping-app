2ecafc05ede7943ec0bd531e420bd2b5da056104
Bashnona Gamal <b.d.fire@hotmail.com>
2020-12-23 21:29:26 +0200

    first-commit

---
+++ ArchProject/IShoppingWebService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ArchProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShoppingWebService" in both code and config file together.
+    [ServiceContract]
+    public interface IShoppingWebService
+    {
+        [OperationContract]
+        void AddItem();
+
+        [OperationContract]
+        void getAllItems();
+
+        [OperationContract]
+        void editItem();
+
+        [OperationContract]
+        void deleteItem();
+
+        [OperationContract]
+        void Register();
+
+        [OperationContract]
+        void login();
+
+        [OperationContract]
+        void getItemById();
+
+    }
+}
