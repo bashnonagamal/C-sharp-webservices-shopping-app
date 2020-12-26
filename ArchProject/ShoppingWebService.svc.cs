8b9f898913037da7122c76f6ae5eac520554b101
Bashnona Gamal <b.d.fire@hotmail.com>
2020-12-26 17:10:15 +0200

    Changes with gitignore

---
+++ ArchProject/ShoppingWebService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ArchProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShoppingWebService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ShoppingWebService.svc or ShoppingWebService.svc.cs at the Solution Explorer and start debugging.
+    public class ShoppingWebService : IShoppingWebService
+    {
+        public void AddItem()
+        {
+
+        }
+        public void getAllItems()
+        {
+
+        }
+        public void editItem()
+        {
+
+        }
+        public void deleteItem()
+        {
+
+        }
+        public void Register()
+        {
+
+        }
+        public void login()
+        {
+
+        }
+        public void getItemById()
+        {
+
+        }
+    }
+}
